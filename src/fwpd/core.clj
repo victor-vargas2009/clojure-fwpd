ebeaed5f5c252ef19b36fa5821a78cd1c73f1760
Victor Vargas Reyes <victorvargasreyes@ucsf.edu>
2020-12-01 09:01:25 -0800

    Added functions for parsing

--- src/fwpd/core.clj
+++ src/fwpd/core.clj
@@ -18,3 +18,29 @@
   [vamp-key value]
   ((get conversions vamp-key) value)
 )
+
+(defn parse
+  "Convert a CSV into rows of columns"
+  [string]
+  (map #(clojure.string/split % #",")
+       (clojure.string/split string #"\n")
+  )
+)
+
+(defn transform-rows
+  [rows]
+  (map (fn [row]
+        (println "Row: " row)) rows)
+)
+
+(transform-rows (parse (slurp filename)))
+
+(defn mapify
+  "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
+  [rows]
+  (map (fn [unmapped-row]
+         (reduce (fn [row-map [vamp-key value]]
+                   (assoc row-map vamp-key (convert vamp-key value)))
+                 {}
+                 (map vector vamp-keys unmapped-row)))
+  rows))
