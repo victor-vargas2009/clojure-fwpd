8b062b87fc577fa43ac479baa2aacccacef7a981
Victor Vargas Reyes <victorvargasreyes@ucsf.edu>
2020-12-15 07:47:27 -0800

    Added new functions to transfor map into array and filter map

--- src/fwpd/core.clj
+++ src/fwpd/core.clj
@@ -1,3 +1,8 @@
+; Turn the result of your glitter filter into a list of names.
+; Write a function, append, which will append a new suspect to your list of suspects.
+; Write a function, validate, which will check that :name and :glitter-index are present when you append. The validate function should accept two arguments: a map of keywords to validating functions, similar to conversions, and the record to be validated.
+; Write a function that will take your list of maps and convert it back to a CSV string. You’ll need to use the clojure.string/join function.
+
 (ns fwpd.core)
 
 ;; Deifnes file
@@ -12,12 +17,18 @@
   (Integer. str))
 
 ;; Conversion for map of vampires
+;; this map is used to associate a conversion function to the value passed
+;; in this case the glitter-index will be converted into an integer
+;; and the name will passed as iss since the identity function just returns the value passed to it
 (def conversions {:name identity :glitter-index str->int})
 
 (defn convert
   [vamp-key value]
   ((get conversions vamp-key) value)
 )
+
+;; This parse function will be used to pass in a CSV and then replace the commas and the new
+;; lines in order to make arrays of each row in the CSV
 
 (defn parse
   "Convert a CSV into rows of columns"
@@ -26,14 +37,6 @@
        (clojure.string/split string #"\n")
   )
 )
-
-(defn transform-rows
-  [rows]
-  (map (fn [row]
-        (println "Row: " row)) rows)
-)
-
-(transform-rows (parse (slurp filename)))
 
 (defn mapify
   "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
@@ -44,3 +47,9 @@
                  {}
                  (map vector vamp-keys unmapped-row)))
   rows))
+
+(defn glitter-filter
+  [minimum-glitter records]
+  (filter #(>= (:glitter-index %) minimum-glitter) records))  
+
+; (glitter-filter 3 (mapify (parse (slurp filename))))
